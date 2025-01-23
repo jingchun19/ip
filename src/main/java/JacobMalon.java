321f21249bcf21a9aba907a701cae41d2f854617
jingchun19 <jingchun19@gmail.com>
2025-01-23 21:55:28 +0800

    Echo created

--- src/main/java/JacobMalon.java
+++ src/main/java/JacobMalon.java
@@ -1,9 +1,26 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class JacobMalon {
+
+    public static void input() {
+        boolean isEnd = false;
+        Scanner scn = new Scanner(System.in);
+        while (!isEnd) {
+            String inputFromUser = scn.nextLine();
+            isEnd = Objects.equals(inputFromUser, "bye");
+            if (isEnd) {
+                break;
+            }
+            System.out.println(inputFromUser);
+        }
+    }
     public static void main(String[] args) {
         String greet = "Hello! I'm JacobMalon \n" +
                 "What can I do for you? \n";
         String exit = "Bye. Hope to see you again!";
         System.out.println(greet);
+        input();
         System.out.println(exit);
     }
 }
